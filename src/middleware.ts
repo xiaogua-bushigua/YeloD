22e644bcc2b31acb82d40121140c5145951b85d8
huanghao_hh <huanghao1@lrhealth.com>
2024-05-23 15:00:18 +0800

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = { matcher: ['/', '/data/:path*', '/queries', '/charts/:path*', '/screens/:path*', '/settings'] };
+
+// testing
