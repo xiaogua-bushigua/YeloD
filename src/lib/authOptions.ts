8cef01c4f2f4cabc8e01ced5c021a7a819281d7f
huanghao_hh <huanghao1@lrhealth.com>
2024-07-11 15:34:05 +0800

    google signIn

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -44,6 +44,7 @@ export const authOptions: AuthOptions = {
 	callbacks: {
 		async signIn(params: { account: Account | null; profile?: Profile | undefined }) {
 			const { account, profile } = params;
+      console.log(account, profile)
 			if (account && account?.provider === 'github') {
 				await dbConnect();
 				try {
